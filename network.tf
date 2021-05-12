7deffcb5b5452a3908a6bc4e2c47cf7bf6a6002f
Sachin Saini <45393031+eddygrant000@users.noreply.github.com>
2021-05-12 12:37:09 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -149,6 +149,11 @@ resource "aws_security_group" "app-group" {
     protocol    = "tcp"
     cidr_blocks = [aws_subnet.ct-subnet-public.cidr_block]
   }
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   egress {
     from_port   = 0
     to_port     = 0
