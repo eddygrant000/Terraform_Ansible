43f9076434d4f62466a7d9a540b0f3a4896ede59
Sachin Saini <45393031+eddygrant000@users.noreply.github.com>
2021-05-12 11:51:52 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,6 +12,6 @@ terraform {
 
 resource "aws_key_pair" "ct-keypair" {
     key_name = "ct-keypair"
-    public_key = file("/home/ubuntu/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh/id_rsa.pub")
     # public_key = file("/home/ubuntu/.ssh/id_rsa.pub")
 }
